17da6ce24757df110ea87dd9cc97899ae627743c
Yusuf-Siddiqui-08 <161784633+Yusuf-Siddiqui-08@users.noreply.github.com>
2024-03-11 21:03:22 +0000

    Added comments explaining code and removed unnecessary lines

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
 print("Loading modules...")
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
-#from sklearn.model_selection import train_test_split
-#from sklearn.metrics import accuracy_score
 
 print("Loading data...")
-data = pd.read_csv('data/train.csv')
-X = data.drop(columns=["PassengerId", "Survived", "Name", "SibSp", "Parch", "Ticket", "Fare", "Cabin", "Embarked"])
-y = data["Survived"]
-#X_train, X_test, y_train, y_test = train_test_split(X,y)
+data = pd.read_csv('data/train.csv') #load dataset to data variable
+X = data.drop(columns=["PassengerId", "Survived", "Name", "SibSp", "Parch", "Ticket", "Fare", "Cabin", "Embarked"]) #set X value to a table of all specified columns
+y = data["Survived"] #set y value to the column of Survived
 
+print("Training model...")
 model = DecisionTreeClassifier()
-#model.fit(X_train,y_train)
-model.fit(X.values,y.values)
+model.fit(X.values,y.values) #train the model
 user_name = input("Enter your name: ")
 user_class = float(input("What passenger class are you (1, 2 or 3): "))
 if user_class != 1 and user_class != 2 and user_class != 3:
@@ -33,6 +30,3 @@ if predictions[0] == 1:
   print(f"You are likely to survive the Titanic! Congratulations {user_name}!")
 else:
   print(f"You are unlikely to survive the Titanic {user_name}!")
-
-#score = accuracy_score(y_test, predictions)
-#print(score*100, "% accuracy.")
