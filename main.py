b59ec2a4b902f970e7d1bbc19c5b6f133f255fbc
Yusuf-Siddiqui-08 <yusufs98783@gmail.com>
2025-01-10 19:15:45 -0500

    added more comments

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print("Loading modules...")
+print("Loading modules...")  # import modules
 import numpy as np
 import pandas as pd
 import os
@@ -6,7 +6,7 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.impute import SimpleImputer
 
 
-def gatherUserInfo():
+def gatherUserInfo():  # function to get user info
     user_name = input("Enter your name: ")
     user_class = int(input("What passenger class are you (1, 2 or 3): "))
     if user_class not in [1, 2, 3]:
@@ -27,30 +27,32 @@ def gatherUserInfo():
     return result
 
 
-def cls():
-    os.system('cls' if os.name == 'nt' else 'clear')
+def cls():  # function to clear console
+    os.system(
+        'cls' if os.name == 'nt' else 'clear')  # has to use different clear commands depending on operating system
 
 
+# main program
 while True:
-    cls()
+    cls()  # clears console
     print("Loading data...")
     data = pd.read_csv('data/train.csv')  # load dataset to data variable
-    imp = SimpleImputer(missing_values=np.nan, strategy='median')
+    imp = SimpleImputer(missing_values=np.nan, strategy='median')  # set imputer to a variable to call on
     X = data.drop(columns=["PassengerId", "Name", "SibSp", "Parch", "Ticket", "Fare", "Cabin",
                            "Embarked"])  # set X to all columns not specified
-    X["Age"] = imp.fit_transform(X[["Age"]]).ravel()
-    np_values = X.values
-    np_new_values = np.delete(np_values, 0, 1)
-    X = pd.DataFrame(np_new_values, columns=["Pclass", "Sex", "Age"])
+    X["Age"] = imp.fit_transform(X[["Age"]]).ravel()  # fill all missing values in age column
+    np_values = X.values  # set X to NumPy array
+    np_new_values = np.delete(np_values, 0, 1)  # Delete unnecessary column
+    X = pd.DataFrame(np_new_values, columns=["Pclass", "Sex", "Age"])  # convert back to pandas dataframe
     y = data["Survived"]  # set y value to the column of Survived
     # compare the data of the x and y values: who survived (y) and what their data was (x)
 
     print("Training model...")
-    model = DecisionTreeClassifier()
+    model = DecisionTreeClassifier()  # set the decision tree (neural network)
     model.fit(X.values, y.values)  # train the model
-    user_data = gatherUserInfo()
-    predictions = model.predict([[user_data[1], user_data[2], user_data[3]]])
-    if predictions[0] == 1:
+    user_data = gatherUserInfo()  # set user info to a variable
+    predictions = model.predict([[user_data[1], user_data[2], user_data[3]]])  # get predictions
+    if predictions[0] == 1:  # print them
         print(f"You are likely to survive the Titanic! Congratulations {user_data[0]}!")
     else:
         print(f"You are unlikely to survive the Titanic {user_data[0]}!")
